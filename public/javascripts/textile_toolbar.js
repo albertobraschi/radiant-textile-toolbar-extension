c01751debf505d8bb199714018dc33d89d6534a1
Matthew Bass <matt@anacreon.local>
2008-11-19 08:54:15 -0500

    added JS to toggle toolbar on and off

--- public/javascripts/textile_toolbar.js
+++ public/javascripts/textile_toolbar.js
@@ -1,5 +1,20 @@
+var TextileToolbar = {
+	toggle:function(id) {
+		if ($(id + '_filter_id').value == 'Textile') {
+			Element.insert(id + '_content', {before:'<div id="textile_toolbar_' + id + '">(textile toolbar)</div>'});
+		} else {
+			if ($('textile_toolbar_' + id))
+				$('textile_toolbar_' + id).remove();
+		}
+	}
+};
+
 Event.addBehavior({
 	'#part_0_filter_id:change' : function(e) {
-		//alert('changed!');
+		TextileToolbar.toggle('part_0');
   }
+});
+
+Event.onReady(function() {
+	TextileToolbar.toggle('part_0');
 });
