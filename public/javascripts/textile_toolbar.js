12da23aca80bb2a674aed86fc81ab53bf8776064
Matthew Bass <matt@anacreon.local>
2008-11-19 02:25:58 -0500

    extension now inserts JS includes into admin pages

---
+++ public/javascripts/textile_toolbar.js
@@ -0,0 +1,5 @@
+Event.addBehavior({
+	'#part_0_filter_id:change' : function(e) {
+		//alert('changed!');
+  }
+});
--- textile_toolbar_extension.rb
+++ textile_toolbar_extension.rb
@@ -1,10 +1,19 @@
 # Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
+require_dependency 'application'
 
 class TextileToolbarExtension < Radiant::Extension
   version "1.0"
   description "Adds toolbars to Textile-enabled text areas in the admin."
   url "http://dev.radiantcms.org/radiant/browser/trunk/extensions/textile-toolbar"
+  
+  def activate
+    Admin::PageController.class_eval do
+      before_filter :set_textile_toolbar_javascript
+      def set_textile_toolbar_javascript
+        %w(lowpro textile_toolbar).each { |e| include_javascript e }
+      end
+    end
+  end
 end
 
 # Soli Deo Gloria!
